ab07a5eea41854dfb3c5a44817bf6f0c093663f0
huda shafique <hudashafique19@gmail.com>
2025-12-28 19:28:47 +0500

    added features

--- analyzer.py
+++ analyzer.py
@@ -12,20 +12,33 @@ for i in range(t_students): #entity
         mark = int(input(f"Enter marks {j + 1}: "))
         marks.append(mark)
 
+    total = sum(marks)
+    avg = total/len(marks)
+    heighest = max(marks)
+    lowest = min(marks)
+
+
     student = {
         "Name": name,
         "Roll no.": roll_no,
-        "Marks": marks
+        "Marks": marks,
+        "total": total,
+        "average": avg,
+        "heighest": heighest,
+        "lowest": lowest
     }
 
     students.append(student)
-
+    
 print("Students data :\n ")
 
 for student in students:
     print("Name:", student["Name"])
     print("Roll No:", student["Roll no."])
     print("Marks:", student["Marks"])
+    print("Total :", student["total"])
+    print("heighest :", student["heighest"])
+    print("lowest :", student["lowest"])
     print("-" * 30)
 
-
+    
